be808fab4ad1962a38da75b032ee86f0f577cafa
davsalgonz <david.slmnc@gmail.com>
2023-04-24 19:07:15 +0200

    spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CoreModule } from "./core/core.module";
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { ApiHashInterceptor } from "./core/interceptors/api-hash.interceptor";
+import { SpinnerInterceptor } from "./core/interceptors/spinner.interceptor";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,9 +18,11 @@ import { ApiHashInterceptor } from "./core/interceptors/api-hash.interceptor";
     BrowserAnimationsModule,
     CoreModule,
     HttpClientModule,
+    SharedModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ApiHashInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/core/interceptors/spinner.interceptor.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, finalize } from "rxjs";
+import { SpinnerService } from "src/app/shared/service/spinner.service";
+
+@Injectable()
+export class SpinnerInterceptor implements HttpInterceptor {
+  constructor(private spinnerService: SpinnerService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    this.spinnerService.show();
+    return next.handle(req).pipe(finalize(() => this.spinnerService.hide()));
+  }
+}
