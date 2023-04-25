03ea45a54eba40f8544417cd09dd3928c024b9b9
JoséSanz <josesanzmartin@hotmail.es>
2023-04-25 12:37:46 +0200

    implementing test

--- src/app/features/services/comic.service.spec.ts
+++ src/app/features/services/comic.service.spec.ts
@@ -1,16 +1,78 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
 
-import { ComicService } from './comic.service';
+import { ComicService } from "./comic.service";
+import { ComicApiResponse } from "../../core/models/IComic";
+import { PriceType } from "src/app/core/models/IPriceType";
 
-describe('ComicService', () => {
+describe("ComicService", () => {
   let service: ComicService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ComicService],
+    });
     service = TestBed.inject(ComicService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("getComics() should return greater than 0", () => {
+    const mockResponse: ComicApiResponse = {
+      code: 200,
+      status: "OK",
+      copyright: "Copyright text",
+      attributionText: "Attribution text",
+      attributionHTML: "<p>Attribution HTML</p>",
+      data: {
+        offset: 0,
+        limit: 20,
+        total: 1,
+        count: 1,
+        results: [
+          {
+            id: 1,
+            title: "Test Comic",
+            description: "This is a test comic.",
+            prices: [
+              {
+                type: PriceType.PrintPrice,
+                price: 3.99,
+              },
+              {
+                type: PriceType.DigitalPurchasePrice,
+                price: 2.99,
+              },
+            ],
+            thumbnail: {
+              path: "http://example.com/test-comic-thumbnail",
+              extension: "jpg",
+            },
+          },
+        ],
+      },
+      etag: "etag_value",
+    };
+
+    service.getComics().subscribe((data) => {
+      expect(data.data.results.length).toBeGreaterThan(0);
+    });
+
+    const req = httpTestingController.expectOne((request) =>
+      request.url.includes("/comics")
+    );
+    req.flush(mockResponse);
   });
 });
