fbe1abc169745cf4a4f9e9312a5abcfe87243357
JoséSanz <josesanzmartin@hotmail.es>
2023-04-26 11:39:49 +0200

    implementing md5

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,16 @@ import { CoreRoutingModule } from "./core-routing.module";
 import { FooterComponent } from "./components/footer/footer.component";
 import { HomeComponent } from "./components/home/home.component";
 import { SharedModule } from "../shared/shared.module";
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { LoginComponent } from "./auth/login/login.component";
 
 @NgModule({
-  declarations: [HomeComponent, FooterComponent, HeaderComponent],
+  declarations: [
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+  ],
   imports: [CommonModule, CoreRoutingModule, SharedModule],
   exports: [FooterComponent, HeaderComponent],
 })
